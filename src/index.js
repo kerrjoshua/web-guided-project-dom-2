d1e51a8ad7a3afce8c337bfe679276877d30ff37
Joshua Kerr <kerrjoshua@gmail.com>
2022-12-07 22:30:50 +0900

    Completed task 6, close the modal on Escape keydown

--- src/index.js
+++ src/index.js
@@ -13,35 +13,60 @@ const failureMessage = document.querySelector('.failure');
 
 // C- MODAL
 const modal = document.querySelector('.modal');
-console.log(modal);
 
 
 // 👉 TASK 2- Demo handling click events on button#launchButton, using:
 //  A- HTML inside index.html
 //  B- The DOM's element.onclick attribute
-launchButton.onclick = () => console.log('Why am I seeing only this?');
+// launchButton.onclick = () => console.log('Why am I seeing only this?');
 //  C- element.addEventListener('click', callback)
-launchButton.addEventListener('click', () => console.log("I'll be seeing this too."))
+// launchButton.addEventListener('click', () => console.log("I'll be seeing this too."))
 
 
 // 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
 
+const launch = () => {
+    modal.classList.remove('off');
+    successMessage.classList.add('off');
+    failureMessage.classList.add('off');
+}
+launchButton.addEventListener('click', launch);
 
 // 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
 // Add it as a listener for clicks on the confirmation button.
+
+const confirm = () => {
+    modal.classList.add('off');
+    successMessage.classList.remove('off');
+}
+
+confirmButton.addEventListener('click', confirm);
 
 
 // 👉 TASK 5- Create a function to cancel the launch.
 // It should close the modal and display a failure report.
 // Add it as a listener for clicks on the cancellation button.
 
+const cancel = () => {
+    modal.classList.add('off');
+    failureMessage.classList.remove('off');
+}
+cancelButton.addEventListener('click', cancel);
 
 // 👉 TASK 6- Create a function that closes the modal if
 // the user hits the Escape key on their keyboard.
 // Add it as an event listener for 'keydown' events on document.
+
+const clearOut = (evt) => {
+if (evt.key = 'Escape') {
+    modal.classList.add('off');
+}
+}
+
+document.querySelector('body').addEventListener('keydown', clearOut);
 
 
 // 👉 TASK 7- Add to ALL ELEMENTS ON THE PAGE an event listener for click events.
